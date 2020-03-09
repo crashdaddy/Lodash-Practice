52c8909b1de5fe2d6c2b6b5308d7f4ad201d039e
crashdaddy <kennethscoggins@mail.com>
2020-03-09 07:57:55 -0500

    added fetch to my fun little RandoAPI for numbers array

--- main.js
+++ main.js
@@ -3,10 +3,21 @@
 const ver = _.VERSION
 console.log("Lodash version: " + ver);  
 
-let unsortedArray = [[87,65,45],34,[76,112],[343,5454],65,[[34,65],23],676,545];
 
-console.log(_.reverse(unsortedArray));
-console.log(_.without(unsortedArray,343));
-console.log(_.shuffle(unsortedArray));
-console.log(_.chunk(unsortedArray,3));
-console.log(_.flattenDeep(unsortedArray));
+const getArray = () => {
+    fetch("https://cors-anywhere.herokuapp.com/https://www.crazyhappyfuntime.com/rando/?length=100&min=1&max=100")
+        .then(res => res.json())
+        .then(returnedArray => {
+            console.log(_.reverse(returnedArray.array));
+            console.log(_.without(returnedArray.array,343));
+            console.log(_.shuffle(returnedArray.array));
+            console.log(_.chunk(returnedArray.array,3));
+            console.log(_.flattenDeep(returnedArray.array));
+
+        }) 
+}
+
+window.onload = ()=> {
+    getArray();
+}
+
